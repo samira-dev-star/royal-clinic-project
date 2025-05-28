bb54c4d62114c9297e9f52dbd289d97e89e7db0d
samira <sammashtizadeh@gmail.com>
2025-05-28 23:57:56 +0330

    serach bar completed for service search

--- royal_clinic/apps/search/views.py
+++ royal_clinic/apps/search/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.views import View
 from django.db.models import Q
 from apps.patient_panel.models import CustomPatient
+from apps.services.models import Services
+
 
 # Create your views here.
 
@@ -11,8 +13,17 @@ class SearchResultsView(View):
         patients = CustomPatient.objects.filter(
             Q(emergency_contact__icontains=query)
         )
+        services = Services.objects.filter(
+            Q(service_title__icontains=query)|
+            Q(service_short_description__icontains=query)|
+            Q(service_description__icontains=query)|
+            Q(slug__icontains=query)|
+            Q(is_available__icontains=query)|
+            Q(procedure_description__icontains=query)
+        )
         context = {
-            "patients":patients,
+            "patients" : patients,
+            "services" : services,
         }
         
         return render(request,"search/search.html",context)
