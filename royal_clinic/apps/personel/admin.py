a7119b61765dcb1c662479f4b5739ae883c6ef63
samira <sammashtizadeh@gmail.com>
2025-05-30 03:57:12 +0330

    feat: Add migrations to alter image fields and integrate social media for personnel and services

--- royal_clinic/apps/personel/admin.py
+++ royal_clinic/apps/personel/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import Personel,PersonelSocialMedia
 # Register your models here.
 
+class PersonelSocialMediaInline(admin.TabularInline):
+    model = PersonelSocialMedia
+    
 
+@admin.register(Personel)
 class PersonelAdmin(admin.ModelAdmin):
-    list_display = ('name_and_family', 'profession','image')
+    list_display = ('show_personel_images','name_and_family', 'profession')
     search_fields = ('name_and_family', 'profession')
     list_filter = ('profession', 'name_and_family')
+    inlines = [PersonelSocialMediaInline]
