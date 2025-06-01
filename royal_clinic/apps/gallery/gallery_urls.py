e9a969d8963cebd85e420de2ed1c4d9eee5ba591
samira <sammashtizadeh@gmail.com>
2025-06-01 14:47:43 +0330

    feat: Implement video gallery with views, templates, and URL routing

--- royal_clinic/apps/gallery/gallery_urls.py
+++ royal_clinic/apps/gallery/gallery_urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'gallery'
 
 urlpatterns = [
-    
+    path('videos/',views.video_list,name='videos'),
+    path('videos_all/',views.video_list_all,name='videos_all'),
+    path('video/<slug:slug>/',views.specific_video,name='video'),
 ]
--- royal_clinic/apps/gallery/views.py
+++ royal_clinic/apps/gallery/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_list_or_404
+from .models import ServiceVideo
 # Create your views here.
+
+
+def video_list(request):
+    
+    videos = ServiceVideo.objects.all()[:3]
+    
+    context = {
+        'videos': videos,
+    }
+    
+    return render(request, 'gallery/partials/gallery_partials.html', context)
+
+# ----------------------------------------------------------------------
+
+def video_list_all(request):
+    
+    videos = ServiceVideo.objects.all()
+    
+    context = {
+        'videos': videos,
+    }
+    
+    return render(request, 'gallery/all_videos.html', context)
+
+# ----------------------------------------------------------------------
+
+def specific_video(request, pk):
+    
+    video = get_list_or_404(ServiceVideo, pk=pk)
+    
+    context = {
+        'video': video,
+    }
+    
+    return render(request, 'partials/gallery_partials.html', context)
