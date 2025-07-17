d608c491969cae262e1a82daa26108b30218ad72
sam_dev_star <sammashtizadeh@gmail.com>
2025-07-17 18:21:09 +0330

    user_panel modified

--- royal_clinic/apps/patient_panel/patient_panel_urls.py
+++ royal_clinic/apps/patient_panel/patient_panel_urls.py
@@ -5,7 +5,9 @@ app_name = 'patient'
 
 urlpatterns = [
     path('patient_profile/', views.CompletePatientProfileView.as_view(), name='patient_profile'),
+    
     path('medical_history_and_allergies/<int:id>/', views.ShowMedicalHistoryAndAllergiesView.as_view(), name='medical_history_and_allergies'),
+    
     path('patients_list/', views.PatientsList.as_view(), name='patients_list'),
     
 ]
