d68a6271e4478bdf765c843a769873d74f290d87
samira-dev-star <sammashtizadeh@gmail.com>
2025-07-23 20:41:05 +0330

    language is changed to persian in settings

--- royal_clinic/royal_clinic/settings.py
+++ royal_clinic/royal_clinic/settings.py
@@ -200,7 +200,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
 TIME_ZONE = 'Asia/Tehran'
 USE_TZ = True
