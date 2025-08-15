0d871d63164473455c81c0b4487e7c5b5028668b
samira-dev-star <sammashtizadeh@gmail.com>
2025-08-15 21:24:51 +0330

    notification app is deleted

--- royal_clinic/apps/notifications/notifications_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'notifications'
-
-urlpatterns = [
-    path('get-admin-notifications/', views.get_admin_notifications, name='get-admin-notifications'),
-]
